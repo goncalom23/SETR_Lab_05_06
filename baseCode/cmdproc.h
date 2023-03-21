d9b2e3b540e373ae5f06121fcece11c2c41bf483
filipempsilva <119332039+filipempsilva@users.noreply.github.com>
2023-03-21 11:06:52 +0000

    Error codes added in cmdproc.h

--- baseCode/cmdproc.h
+++ baseCode/cmdproc.h
@@ -7,6 +7,14 @@
 #define MAX_CMDSTRING_SIZE 10 /* Maximum size of the command string */ 
 #define SOF_SYM '#'	          /* Start of Frame Symbol */
 #define EOF_SYM '!'           /* End of Frame Symbol */
+#define Exit_Successful 0
+#define EMPTY_STRING -1
+#define invalid_command_found -2
+#define CS_error_detected -3
+#define wrong_string_format  -4
+#define INCOMPLETE_COMMAND -5
+#define FULL_STRING -6
+
 
 /* Function prototypes */
 int cmdProcessor(void);
